a5a5d8bdb788342df6e27595d427a5eb7c728f3e
bijucksae <jiwonmin000@gmail.com>
2024-11-05 16:00:49 +0900

    알까기 기능 추가

--- sketch.js
+++ sketch.js
@@ -18,7 +18,19 @@ function draw() {
  // ball.addForce(wind);
   //}
 
+  
+  ball.pos
+  
+
   // 파티클 업데이트 및 표시
   ball.update();
   ball.show();
+}
+
+function mouseClicked() {
+//let took = createVector();
+let mPos = createVector(mouseX, mouseY);
+let took = p5.Vector.sub(ball.pos, mPos);
+took.mult(0.1);
+ball.addForce(took);
 }
